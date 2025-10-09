e692798acff0ebdc4ad4744895b4861d714a336f
Jarrett Croll <jarrett.croll@gmail.com>
2025-10-09 10:57:54 -0400

    inventory controller

---
+++ api/src/Controller/InventoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class InventoryController extends AbstractController
+{
+    #[Route('/inventory', name: 'api_inventory')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            [
+                'name' => 'Apple iPhone 17 Pro Max',
+                'inventory' => 14,
+            ],
+        ]);
+    }
+}
